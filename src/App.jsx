3a09942f92d7b295b84313ddf8128401b99d3963
ChinmayP4526 <chinmaypatilr4526@gmail.com>
2026-04-14 18:37:19 +0530

    Installed Utilities

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import gsap from 'gsap'
 import './App.css'
-function App() {
- 
+import { ScrollTrigger,SplitText } from 'gsap/all'
 
-  return (
+gsap.registerPlugin(ScrollTrigger,SplitText)
+function App() {
+   return (
     <>
-      
+      <h1 className='text-3xl text-indigo-300'>Hello Gsap</h1>
     </>
   )
 }
